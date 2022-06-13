0d5078f4e994e33b48feef19ca22af65ab21939a
Bilal Cetiner <contact@bilalcetiner.com.tr>
2022-06-13 03:41:27 +0300

    Added catalog api changes

--- Services/CatalogAPI/Program.cs
+++ Services/CatalogAPI/Program.cs
@@ -3,15 +3,32 @@ using CatalogAPI.Business.Abstract;
 using CatalogAPI.Business.Concrete;
 using CatalogAPI.Services;
 using CatalogAPI.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add Jwt Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    // Token Issuer
+    options.Authority = builder.Configuration.GetSection("IdentityServerUrl").Value;
+    options.Audience = "resource_catalog";
+    options.RequireHttpsMetadata = false;
+});
+
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => 
+{
+    // Add authorize to all controllers
+    options.Filters.Add(new AuthorizeFilter());
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.Configure<DataBaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
 // Business
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 builder.Services.AddScoped<ICourseService, CourseManager>();
@@ -30,9 +47,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
