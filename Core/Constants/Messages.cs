97a1bcb8df65cd414b588b168285606695bef155
Bilal Cetiner <contact@bilalcetiner.com.tr>
2022-06-13 03:37:05 +0300

    Core layer configuration

---
+++ Core/Constants/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Constants
+{
+    public class Messages
+    {
+        //Record messages
+        public static string RecordsAdded = "Records successfully added";
+        public static string RecordsListed = "Records successfully listed";
+        public static string RecordGetted = "Record successfully getted";
+        public static string RecordsUpdated = "Records successfully updated";
+        public static string RecordsDeleted = "Records successfully deleted";
+        public static string RecordNotFount = "Record not found.";
+
+        public static string PhotoSaved = "Photo successfully saved";
+        public static string PhotoCouldNotSaved = "Photo could not be saved";
+        public static string PhotoDeleted = "Photo deleted";
+    }
+}
