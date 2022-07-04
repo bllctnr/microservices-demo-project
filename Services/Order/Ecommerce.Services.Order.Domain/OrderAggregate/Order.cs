0ce1c091b59a951a1162c887d4bcf5f8a217727d
Bilal Cetiner <contact@bilalcetiner.com.tr>
2022-07-04 15:24:11 +0300

    Added Order aggregate on domain

---
+++ Services/Order/Ecommerce.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services.Order.Domain.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+        public Address Address { get; private set; }
+        public string CustomerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItems; // Backing field
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+
+        public Order(string customerId, Address address) 
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            CustomerId = customerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId, string productName, decimal price, string photoUrl) 
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+            if (!existProduct) 
+            {
+                var newOrderItem = new OrderItem(productId, productName, photoUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+    }
+
+    // EF Core features
+    // - Owned Types
+    // - Shadow Property
+    // - Backing Field
+}
---
+++ Services/Order/Ecommerce.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PhotoUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public OrderItem(string productId, string productName, string photoUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PhotoUrl = photoUrl;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string productName, string photoUrl, decimal price) 
+        {
+            ProductName = productName;
+            PhotoUrl = photoUrl;
+            Price = price;
+        }
+    }
+}
