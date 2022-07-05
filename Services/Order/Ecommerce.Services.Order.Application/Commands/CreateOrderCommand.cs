658408a45495f43d2947f68084378876d6750a94
Bilal Cetiner <contact@bilalcetiner.com.tr>
2022-07-05 15:11:18 +0300

    Added Command and command handler

---
+++ Services/Order/Ecommerce.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using Core.Results;
+using Ecommerce.Services.Order.Application.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services.Order.Application.Commands
+{
+    public class CreateOrderCommand : IRequest<JsonDataResult<CreatedOrderDto>>
+    {
+        public string CustomerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public AddressDto AddressDto { get; set; }
+    }
+}
---
+++ Services/Order/Ecommerce.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using Core.Constants;
+using Core.Results;
+using Ecommerce.Services.Order.Application.Commands;
+using Ecommerce.Services.Order.Application.Dtos;
+using Ecommerce.Services.Order.Domain.OrderAggregate;
+using Ecommerce.Services.Order.Infrastructure;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, JsonDataResult<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _context;
+        public CreateOrderCommandHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<JsonDataResult<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var newAddress = new Address(
+                request.AddressDto.Province, 
+                request.AddressDto.District, 
+                request.AddressDto.Street, 
+                request.AddressDto.ZipCode, 
+                request.AddressDto.Line
+                );
+
+            Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(request.CustomerId, newAddress);
+
+            request.OrderItems.ForEach(x =>
+            {
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PhotoUrl);
+            });
+
+            var result = await _context.SaveChangesAsync();
+
+            return new SuccessJsonDataResult<CreatedOrderDto>(new CreatedOrderDto {OrderId = newOrder.Id }, Messages.RecordsAdded);
+        }
+    }
+}
