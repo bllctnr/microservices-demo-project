6ff4991859024f783e608e9700f0b2430682aa4e
Bilal Cetiner <contact@bilalcetiner.com.tr>
2022-07-09 19:29:43 +0300

    Added mass transit & rabbitmq configuration on paymnet api

--- Services/PaymentAPI/Program.cs
+++ Services/PaymentAPI/Program.cs
@@ -1,10 +1,10 @@
+using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build(); // Create Policy
@@ -17,6 +17,17 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     options.RequireHttpsMetadata = false;
 });
 
+// Mass transit & Rabbit MQ configuration
+builder.Services.AddMassTransit(x => {
+    // Default Port : 5672
+    x.UsingRabbitMq((context, cfg) => {
+        cfg.Host(builder.Configuration.GetSection("RabbitMQUrl").Value, "/", host => {
+            host.Username("guest");
+            host.Password("guest");
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
